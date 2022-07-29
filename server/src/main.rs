1465b323c4225e5af43e4b7ffd572554e1a53ae8
obiwac <obiwac@gmail.com>
2022-07-29 17:22:02 +0200

    server: User info API endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,34 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::net::SocketAddr;
 
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
-use serde_json::{Value};
+use serde::Deserialize;
+use serde_json::{Value, json};
 
 const DB_PATH: &str = "db.json";
 const API_VERSION: &str = "1.0";
 
-fn not_found() -> Response<Body> {
+#[derive(Deserialize, Debug)]
+struct DBUser {
+	klapgijp: String
+}
+
+fn not_found(msg: String) -> Response<Body> {
 	Response::builder()
 		.status(StatusCode::NOT_FOUND)
-		.body("404 Not found".into())
+		.body(format!("404 Not found: {}", msg).into())
 		.unwrap()
 }
 
-async fn respond(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+async fn respond(req: Request<Body>, db: Value) -> Result<Response<Body>, hyper::Error> {
 	let split = req.uri().path().split("/");
 	let bits: Vec<&str> = split.collect();
 
 	if bits[0] != "" {
-		return Ok(not_found());
+		return Ok(not_found("invalid HTTP GET request format".into()));
 	}
 
 	match (req.method(), bits[1]) {
@@ -29,15 +36,23 @@ async fn respond(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 			Ok(Response::new(Body::from(API_VERSION)))
 		}
 
-		(&Method::GET, "users") => {
-			let sort = String::from(bits[2]);
-			println!("{}", sort);
+		(&Method::GET, "user_info") => {
+			let user = String::from(bits[2]);
+			println!("Requesting information about user '{}'", user);
 
-			Ok(Response::new(Body::from(sort)))
+			// XXX this is a very temporary solution
+
+			let users: HashMap<String, Value> = serde_json::from_str(&db["users"].to_string()).unwrap();
+
+			if ! users.contains_key(&user) {
+				return Ok(not_found(format!("user {} doesn't exist", user)));
+			}
+
+			Ok(Response::new(Body::from(users[&user].to_string())))
 		}
 
 		_ => {
-			Ok(not_found())
+			Ok(not_found(format!("unknown API endpoint: {}", bits[1])))
 		}
 	}
 }
@@ -54,16 +69,15 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 		}
 		Err(err) => {
 			eprintln!("Failed to open {} for reading: {}", DB_PATH, err);
-			serde_json::from_str("{}")?
+			json!({})
 		}
 	};
 
-	println!("{}", db);
-
 	// create server
 
-	let make_service = make_service_fn(|_conn| async {
-		Ok::<_, hyper::Error>(service_fn(respond))
+	let make_service = make_service_fn(move |_conn| {
+		let db = db.clone();
+		async move { Ok::<_, hyper::Error>(service_fn(move |req| respond(req, db.clone()))) }
 	});
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 6969));
