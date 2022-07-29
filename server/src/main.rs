89e239d551cada38441121d627d0402cd6ccdb6e
obiwac <obiwac@gmail.com>
2022-07-29 13:53:26 +0200

    server: Version API endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,36 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
-async fn responder(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-	Ok(Response::new(Body::from("Test response")))
+const API_VERSION: &str = "1.0";
+
+async fn respond(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+	let mut not_found = Response::default();
+	*not_found.status_mut() = StatusCode::NOT_FOUND;
+
+	let split = req.uri().path().split("/");
+	let bits: Vec<&str> = split.collect();
+
+	if bits[0] != "" {
+		return Ok(not_found);
+	}
+
+	match (req.method(), bits[1]) {
+		(&Method::GET, "version") => {
+			Ok(Response::new(Body::from(API_VERSION)))
+		}
+
+		_ => {
+			Ok(not_found)
+		}
+	}
 }
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 	let make_service = make_service_fn(|_conn| async {
-		Ok::<_, Infallible>(service_fn(responder))
+		Ok::<_, hyper::Error>(service_fn(respond))
 	});
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 6969));
